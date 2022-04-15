7afa89ed44b938b13d70ed4a501c60fc03d4f33b
digitaldeja0 <90660363+digitaldeja0@users.noreply.github.com>
2022-04-15 19:17:46 -0400

    Add JS vars

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-
+let minuetes = 00;
+let seconds = 00;
+let appendTens = document.querySelector("#minuets");
+let appendSeconds = document.querySelector("#seconds");
+let startBtn = document.querySelector("#button-start");
+let endBtn = document.querySelector("#button-end");
+let resteBtn = document.querySelector("#button-reset");
