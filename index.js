32e8accf4950461ed1c34868c393d34fe2c1ce4b
digitaldeja0 <90660363+digitaldeja0@users.noreply.github.com>
2022-04-15 19:44:28 -0400

    Update javascript

--- index.js
+++ index.js
@@ -1,7 +1,46 @@
-let minuetes = 00;
+// Variables
+let tens = 00;
 let seconds = 00;
-let appendTens = document.querySelector("#minuets");
+let appendTens = document.querySelector("#tens");
 let appendSeconds = document.querySelector("#seconds");
 let startBtn = document.querySelector("#button-start");
-let endBtn = document.querySelector("#button-end");
-let resteBtn = document.querySelector("#button-reset");
+let stopBtn = document.querySelector("#button-stop");
+let resetBtn = document.querySelector("#button-reset");
+
+// Functions
+
+function startTimer() {
+  tens++;
+
+  if (tens < 10) {
+    appendTens.innerHTML = "0" + tens;
+  }
+  if (tens > 9) {
+    appendTens.innerHTML = tens;
+  }
+  if (tens > 59) {
+    seconds++;
+    appendSeconds.innerHTML = "0" + seconds;
+    tens = "0";
+    appendTens.innerHTML = "0" + 0;
+  }
+  if (seconds > 9) {
+    appendSeconds.innerHTML = seconds;
+  }
+}
+
+startBtn.addEventListener("click", () => {
+  interval = setInterval(startTimer, 1000);
+});
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(interval);
+});
+
+resetBtn.addEventListener("click", () => {
+  clearInterval(interval);
+  tens = "00";
+  seconds = "00";
+  appendTens.innerHTML = tens;
+  appendSeconds.innerHTML = seconds;
+});
